b3ea882b5feb6401ed8fe6fe04ed1d823f31cefa
Wonyoung Cho <bourbaki10@gmail.com>
2020-12-08 15:51:46 +0900

    Add files via upload

---
+++ formatData.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+
+
+def FormatData(path, sep = '\t', chromosome = 'chr', p_value = 'p_wald'):
+    data = pd.read_table(path, sep = sep)
+    data['-log10(p_value)'] = -np.log10(data[p_value])
+    data[chromosome] = data[chromosome].astype('category')
+    data['ind'] = range(len(data))
+    data_grouped = data.groupby((chromosome))
+    return data, data_grouped
+
