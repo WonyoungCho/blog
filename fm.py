3b545e2a1b09128d74116ea8d36a151f6c5357f8
Wonyoung Cho <bourbaki10@gmail.com>
2020-12-09 14:58:45 +0900

    Create fm.py

---
+++ fm.py
@@ -0,0 +1,13 @@
+## Find some fname1's words in fname2
+
+#! /usr/bin/env python3
+
+import subprocess
+import sys
+
+fname1=sys.argv[1]
+column=sys.argv[2]
+fname2=sys.argv[3]
+
+cmd='awk \'{print $'+column+'}\' '+fname1+'|while read line;do grep $line '+fname2+';done|column -ts $\'\\t\''
+subprocess.run(cmd,shell=True)
